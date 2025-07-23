51debf5e5a953a9940f68bc44b36655dffcc32e2
HKMUSIC <himanshu4hk9@gmail.com>
2025-07-23 23:35:16 +0530

    Update couples.py

--- NOBITA/plugins/tools/couples.py
+++ NOBITA/plugins/tools/couples.py
@@ -15,7 +15,7 @@ POLICE = [
     [
         InlineKeyboardButton(
             text="• ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ •",
-            url=f"https://t.me/NOBITA_MUSIC_ROBOT?startgroup=true",
+            url=f"https://t.me/Music_promaxbot?startgroup=true",
         ),
     ],
 ]
